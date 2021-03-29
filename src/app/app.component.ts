e74d119a5fdd4be533d15084be5506b60a1549e8
marcusungarette <marcusungarette@gmail.com>
2021-03-29 18:56:39 -0300

    Added Faker Api; HTML start elements and CSS Bulma configs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { lorem } from 'faker';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'typing-challenge';
+  randomText = lorem.sentence();
 }
